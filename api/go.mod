308063be414bb8748850439885cac55bf7dade6e
OTakumi <takumi.o.0410@gmail.com>
2024-12-17 18:49:42 +0900

    add files: go.mod, main.go

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/OTakumi/just-do-it-api
+
+go 1.23.4
---
+++ api/main.go
@@ -0,0 +1,16 @@
+package main
+
+type Task struct {
+	ID        string `json:"id"`
+	Title     string `json:"title"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+func main() {
+	println("Hello World")
+
+	var sample_task = Task{ID: "1", Title: "Sample", CreatedAt: "Today", UpdatedAt: "Today"}
+
+	println(sample_task.ID, sample_task.Title)
+}
