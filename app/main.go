e42cc72b73bd9c28d64a30067c8751771cba5beb
OTakumi <takumi.o.0410@gmail.com>
2024-12-29 17:34:32 +0900

    feat: create a table, insert and get tasks This commit adds the table creation, insert, and get tasks from the database.

--- app/main.go
+++ app/main.go
@@ -1,20 +1,72 @@
 package main
 
-type Task struct {
-	ID        string `json:"id"`
-	Title     string `json:"title"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
-}
+import (
+	"database/sql"
+	"fmt"
 
-func (t *Task) Create() {
-	println("Create Task")
+	_ "github.com/lib/pq"
+)
+
+type Task struct {
+	ID   int
+	Name string
 }
 
 func main() {
-	println("Hello World")
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=myuser password=mypassword dbname=mydb sslmode=disable")
+	defer db.Close()
 
-	var sample_task = &Task{ID: "1", Title: "Sample", CreatedAt: "Today", UpdatedAt: "Today"}
+	if err != nil {
+		panic(err.Error())
+	}
 
-	println(sample_task.ID, sample_task.Title)
+	fmt.Println("Successfully connected to the database!")
+	fmt.Println("Starting the application...")
+
+	// Create a table
+	createTable(db)
+
+	// Insert a task
+	task := Task{Name: "Learn Go"}
+	insertTask(db, task)
+
+	// Get all tasks
+	tasks := getTasks(db)
+	for _, task := range tasks {
+		fmt.Println(task)
+	}
+}
+
+func createTable(db *sql.DB) {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS tasks (id SERIAL PRIMARY KEY, name TEXT)")
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
+func getTasks(db *sql.DB) []Task {
+	rows, err := db.Query("SELECT * FROM tasks")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	var tasks []Task
+	for rows.Next() {
+		var task Task
+		err := rows.Scan(&task.ID, &task.Name)
+		if err != nil {
+			panic(err.Error())
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks
+}
+
+func insertTask(db *sql.DB, task Task) {
+	_, err := db.Exec("INSERT INTO tasks(name) VALUES($1)", task.Name)
+	if err != nil {
+		panic(err.Error())
+	}
 }
